8ed53b00be9cf32c3f34c47a8cd42f35ab623ecb
Andrei <Andrei2000211@mail.ru>
2023-11-22 05:09:10 +0300

    Fixs

--- ShopApp/views.py
+++ ShopApp/views.py
@@ -1,21 +1,33 @@
 from django.shortcuts import render
-
+import random
 # Create your views here.
+test_list = [['Первый предмет', 'imtemscr', ''],
+                 ['Второй предмет', 'imtemscr', ''],
+                 ['Третий предмет', 'imtemscr', ''],
+                 ['Четвёртый предмет', 'imtemscr', ''],
+                 ['Новый предмет', 'imtemscr', ''],
+                 ['Новый предмет', 'imtemscr', ''],
+                 ['Новый предмет', 'imtemscr', ''],
+                 ['Новый предмет c очень длинным текстом который надо как то настроить', 'imtemscr', ''],
+                 ['Предмет не знаю какой уже', 'imtemscr', ''],
+                 ['Айтем который давно выбран и есть', 'imtemscr', ''],
+                 ['Такой то тут предмет', 'imtemscr', ''],
+                 ['Просто новый предмет для тебя', 'imtemscr', ''],
+                 ['Оно тебе не надо', 'imtemscr', ''],
+                 ['Остаточный предмет', 'imtemscr', ''],
+                 ['Новый текст для предмета c очень длинным текстом', 'imtemscr', '']]
 
 
 def index(request):
-    return render(request, "index.html")
+
+    shuffle_list = test_list[:]
+    random.shuffle(shuffle_list)
+
+    return render(request, "index.html", context= {'data': shuffle_list })
 
 
 def PageUrl_2(request):
-    return render(request, "PageUrl2.html", context={'data': [['Первый предмет', 'itemscr'],
-                                                              ['Второй предмет', 'imtemscr'],
-                                                              ['Третий предмет', 'imtemscr'],
-                                                              ['Четвёртый предмет', 'imtemscr'],
-                                                              ['Новый предмет', 'imtemscr'],
-                                                              ['Новый предмет', 'imtemscr'],
-                                                              ['Новый предмет', 'imtemscr'],
-                                                              ['Новый предмет', 'imtemscr']]})
+    return render(request, "PageUrl2.html", context={'data': test_list})
 
 
 def PageUrl_3(request):
